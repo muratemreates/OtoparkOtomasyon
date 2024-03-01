b3458188d39ca492bd5493485fd4071c7f80963e
Murat Emre Ates <m.emre6530@gmail.com>
2024-03-01 13:29:55 +0300

    FluentValidation eklendi .Yanlış açılan Formlar silindi

---
+++ Bussiness/Validationrules/Fulentvalidation/CarValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Bussiness.Validationrules.Fulentvalidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Surname).NotEmpty();
+            RuleFor(p => p.CitizenshipNumber).NotEmpty();
+            RuleFor(p => p.CarBrand).NotEmpty();
+            RuleFor(p => p.NumberPlate).NotEmpty();
+            RuleFor(p => p.Gsm).NotEmpty();
+            RuleFor(p => p.CarPark).NotEmpty();
+            
+        }
+    }
+}
--- PARKOT.WindowsFormUI/Form1.cs
+++ PARKOT.WindowsFormUI/Form1.cs
@@ -2,21 +2,25 @@
 using DataAccess.Concrete;
 using System;
 using System.Windows.Forms;
+using Bussiness.DependencyResolvers.Ninject;
 
 namespace PARKOT.WindowsFormUI
 {
     public partial class Form1 : Form
     {
-        private ICarService _carService;
+        private readonly ICarService _carService;
+        private readonly IMemberService _memberService;
         public Form1()
         {
             InitializeComponent();
+            _carService = InstanceFactory.GetIstance<ICarService>();
+            _memberService = InstanceFactory.GetIstance<IMemberService>();
         }
 
-        readonly EfCarDal efCarDal  = new EfCarDal();
+      
         private void Form1_Load(object sender, EventArgs e)
         {
-            dgw_Cars.DataSource = efCarDal.GetAll();
+            dgw_Cars.DataSource = _carService.GetAll();
         }
     }
 }
